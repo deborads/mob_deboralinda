373a4340e7f3153a919a36592111b23caba226e2
deborads <168208454+deborads@users.noreply.github.com>
2026-09-22 20:43:25 +0000

    exercicio 4 feito

--- app-laboratorio/App.js
+++ app-laboratorio/App.js
@@ -4,14 +4,25 @@ const eu = {
   nome: 'Débora Dias',
   idade: 17,
   cidade: 'Cascavel',
+  nota1: 8,
+  nota2: 7,
 };
 
+function calcularMedia(aluno) {
+  return (aluno.nota1 + aluno.nota2) / 2;
+}
+
 export default function App() {
+  const media = calcularMedia(eu);
+
   return (
     <View style={styles.container}>
       <Text>Nome: {eu.nome}</Text>
       <Text>Idade: {eu.idade}</Text>
       <Text>Cidade: {eu.cidade}</Text>
+      <Text>Nota 1: {eu.nota1}</Text>
+      <Text>Nota 2: {eu.nota2}</Text>
+      <Text>Média: {media}</Text>
     </View>
   );
 }
@@ -19,7 +30,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#57f0b8b4',
+    backgroundColor: '#d06fd7dc',
     alignItems: 'center',
     justifyContent: 'center',
   },
